ca3136dc0f2cc68bffb546083bfc8bb248906835
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-05-29 21:03:53 -0400

    Moved graph to new file. setUp method in test.

--- Test_BreadthFirstSearch.py
+++ Test_BreadthFirstSearch.py
@@ -1,31 +1,17 @@
 import unittest
+from Graph import Graph
 
 
-class Node:
-    def __init__(self, name):
-        self.name = name
+class BreadthFirstSearch(unittest.TestCase):
+    def setUp(self):
+        self.graph = Graph()
 
-
-class Graph:
-    def __init__(self):
-        self.nodes = []
-
-    def __len__(self):
-        return len(self.nodes)
-
-    def createNode(self, name):
-        self.nodes.append(Node(name))
-
-
-class MyTestCase(unittest.TestCase):
     def test_makeEmptyGraph(self):
-        graph = Graph()
-        self.assertTrue(len(graph) == 0)
+        self.assertTrue(len(self.graph) == 0)
 
     def test_makeGraphOfOneNode(self):
-        graph = Graph()
-        graph.createNode("A")
-        self.assertTrue(len(graph) == 1)
+        self.graph.createNode("A")
+        self.assertTrue(len(self.graph) == 1)
 
 
 if __name__ == '__main__':
